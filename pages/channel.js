6b3bc99dab1a1b6a57a286458eac4b040c4c9298
jhonned01 <jhonned01@gmail.com>
2020-11-11 21:35:37 -0500

    aque agregue el podcas js

--- pages/channel.js
+++ pages/channel.js
@@ -1,90 +1,114 @@
 import React from 'react'
-import {useRouter} from 'next/router'
-
+import Link from 'next/link'
 
 export default function channel(props) {
 
 
 
-      const {channel,audioClips,series} =props
+      const {channel,audioClips,series}=props
     return (
-        <div>
-        <header>Podcasts</header>
+      <div>
+         <header>Podcasts</header>
 
-    <h1>{channel.title}</h1>
-       
+        <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
+
+        <h1>{ channel.title }</h1>
+
+       { series.length > 0 &&
+        <div>
+          <h2>Series</h2>
+          <div className="channels">
+            { series.map((serie) => (
+              <Link href={`/channel?id=${ serie.id }`} >
+                <a className="channel">
+                  <h2>{ serie.title }</h2>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+        }
+      
+      
+      <h2>ultimos Podcasts</h2>
+      
         {
       audioClips.map((clip)=>(
-        <div key={clip.id}>{clip.title}</div>
+        <Link href={`/podcast?id=${clip.id}`} prefetch key={clip.id}>
+        <a className='podcast'>
+          <h3>{ clip.title }</h3>
+          <div className='meta'>
+            { Math.ceil(clip.duration / 60) } minutes
+          </div>
+        </a>
+      </Link>
       ))
     }
    
        
 
     <style jsx>{`
-        header {
-          color: #fff;
-          background: #8756ca;
-          padding: 15px;
-          text-align: center;
-        }
+header {
+color: #fff;
+background: #8756ca;
+padding: 15px;
+text-align: center;
+}
+.banner {
+  width: 100%;
+  padding-bottom: 25%;
+  background-position: 50% 50%;
+  background-size: cover;
+  background-color: #aaa;
+}
 
-        .banner {
-          width: 100%;
-          padding-bottom: 25%;
-          background-position: 50% 50%;
-          background-size: cover;
-          background-color: #aaa;
+.channels {
+  display: grid;
+  grid-gap: 15px;
+  padding: 15px;
+  grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+}
+a.channel {
+  display: block;
+  margin-bottom: 0.5em;
+  color: #333;
+  text-decoration: none;
+}
+.channel img {
+  border-radius: 3px;
+  box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+  width: 100%;
+}
+h1 {
+  font-weight: 600;
+  padding: 15px;
+}
+h2 {
+  padding: 5px;
+  font-size: 0.9em;
+  font-weight: 600;
+  margin: 0;
+  text-align: center;
         }
-
-        .channels {
-          display: grid;
-          grid-gap: 15px;
-          padding: 15px;
-          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+.podcast {
+  display: block;
+  text-decoration: none;
+  color: #333;
+  padding: 15px;
+  border-bottom: 1px solid rgba(0,0,0,0.2);
+  cursor: pointer;
         }
-        a.channel {
-          display: block;
-          margin-bottom: 0.5em;
-          color: #333;
-          text-decoration: none;
-        }
-        .channel img {
-          border-radius: 3px;
-          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
-          width: 100%;
-        }
-        h1 {
-          font-weight: 600;
-          padding: 15px;
-        }
-        h2 {
-          padding: 5px;
-          font-size: 0.9em;
-          font-weight: 600;
-          margin: 0;
-          text-align: center;
-        }
-
-        .podcast {
-          display: block;
-          text-decoration: none;
-          color: #333;
-          padding: 15px;
-          border-bottom: 1px solid rgba(0,0,0,0.2);
-          cursor: pointer;
-        }
-        .podcast:hover {
+.podcast:hover {
           color: #000;
         }
-        .podcast h3 {
+.podcast h3 {
           margin: 0;
-        }
-        .podcast .meta {
-          color: #666;
-          margin-top: 0.5em;
-          font-size: 0.8em;
-        }
+}
+.podcast .meta {
+  color: #666;
+  margin-top: 0.5em;
+  font-size: 0.8em;
+}
       `}</style>
 
       <style jsx global>{`
@@ -104,15 +128,18 @@ export default function channel(props) {
 export async function getServerSideProps({query}){
     const idChannel = query.id
 
-    const resChannel= await fetch(`https://api.audioboom.com/channels/${idChannel}`)
+    const [resChannel,reqAudios,reqSeries]= await Promise.all([
+      fetch(`https://api.audioboom.com/channels/${idChannel}`),
+      fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`),
+      fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
+    ])
+
     const dataChannel = await resChannel.json()
     const channel= await dataChannel.body.channel
 
-    const reqAudios=await fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
     const dataAudios = await reqAudios.json()
     const audioClips = await dataAudios.body.audio_clips
 
-    const reqSeries=await fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`)
     const dataSeries = await reqSeries.json()
     const series = await dataSeries.body.channels
 
---
+++ pages/podcast.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import Link from 'next/link'
+export default function podcast(props) {
+
+    const { clip } = props
+    return (
+        <div>
+      <header>Podcasts</header>
+
+      <div className='modal'>
+        <div className='clip'>
+          <nav>
+            <Link href={`/channel?id=${clip.channel.id}`}>
+              <a className='close'>&lt; Volver</a>
+            </Link>
+          </nav>
+
+          <picture>
+            <div style={{ backgroundImage: `url(${clip.urls.image || clip.channel.urls.logo_image.original})` }} />
+          </picture>
+
+          <div className='player'>
+            <h3>{ clip.title }</h3>
+            <h6>{ clip.channel.title }</h6>
+            <audio controls autoPlay={false}>
+              <source src={clip.urls.high_mp3} type='audio/mpeg' />
+            </audio>
+          </div>
+        </div>
+      </div>
+
+      <style jsx>{`
+        nav {
+          background: none;
+        }
+        nav a {
+          display: inline-block;
+          padding: 15px;
+          color: white;
+          cursor: pointer;
+          text-decoration: none;
+        }
+        .clip {
+          display: flex;
+          height: 100%;
+          flex-direction: column;
+          background: #0f0c29;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+          color: white;
+        }
+        picture {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex: 1 1;
+          flex-direction: column;
+          width: auto;
+          padding: 10%;
+        }
+        picture div {
+          width: 100%;
+          height: 100%;
+          background-position: 50% 50%;
+          background-size: contain;
+          background-repeat: no-repeat;
+        }
+        .player {
+          padding: 30px;
+          background: rgba(0,0,0,0.3);
+          text-align: center;
+        }
+        h3 {
+          margin: 0;
+        }
+        h6 {
+          margin: 0;
+          margin-top: 1em;
+        }
+        audio {
+          margin-top: 2em;
+          width: 100%;
+        }
+
+        .modal {
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          z-index: 99999;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: system-ui;
+          background: white;
+        }
+      `}</style>
+    </div>
+
+    )
+}
+
+export async function getServerSideProps({query}){
+    const id = query.id
+    const fetchClip = await fetch(`https://api.audioboom.com/audio_clips/${id}.mp3`)
+    const clip = (await fetchClip.json()).body.audio_clip
+    return {props:{ clip }}
+  }
